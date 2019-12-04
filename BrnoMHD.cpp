47bcde51ad29268ddf71273f792e707efcc45fc3
Saytam313 <50132863+Saytam313@users.noreply.github.com>
2019-12-04 16:42:03 +0100

    first init

---
+++ BrnoMHD.cpp
@@ -0,0 +1,23 @@
+#include<simlib.h>
+
+#define POCET_OBYVATEL 380 681
+
+#define CO_STOPA_AUTO 144.1
+
+#define CO_STOPA_SALINA 35.1
+
+#define CO_STOPA_AUTOBUS 104.7
+
+#define PROCENTO_RIDICI 43
+
+#define PROCENTO_MHD 38
+
+#define PROCENTO_PESKY 18
+
+#define KAPACITA_SALINA 162
+
+#define VZDALENOST_PRACE 12
+
+int main(int argc, char *argv[]){
+	
+}
